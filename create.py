f7a99ab384ea14d2704a0acbc7f33386d664d20e
VR <patel486@purdue.edu>
2019-08-15 04:19:55 -0400

    renamed && repurpose
    Dedicated file to create a table in DynamoDB and printing status in cli

---
+++ create.py
@@ -0,0 +1,44 @@
+import boto3
+
+
+dynamodb = boto3.client('dynamodb', region_name = 'us-west-2')
+#	table = dynamodb.Table('stock_table')
+try:
+	table = dynamodb.create_table(
+		TableName = 'stock_table',
+		KeySchema =[
+			{
+				'AttributeName': 'Ticker',
+				'KeyType' : 'HASH'
+			},
+			{
+				'AttributeName': 'Stock Price',
+				'KeyType': 'RANGE'
+			}
+		],
+		AttributeDefinitions=[
+			{
+				'AttributeName': 'Ticker',
+				'AttributeType': 'S',
+			},
+			{
+				'AttributeName': 'Stock Price',
+				'AttributeType': 'N'
+			},
+		],
+		ProvisionedThroughput={
+	    	'ReadCapacityUnits': 10,
+	    	'WriteCapacityUnits': 10
+	    }
+	)
+except dynamodb.exceptions.ResourceInUseException:
+	pass
+#	table = dynamodb.Table('stock_table')
+print("Status: ", boto3.resource('dynamodb', region_name = 'us-west-2').Table('stock_table').table_status)
+# table.put_item(
+# 	Item={
+# 		'Ticker': 'SPY',
+# 		'Cost': 288,
+# 		'Sentiment': 'Bearish'
+# 	}
+# )
